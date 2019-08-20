2367dff0d5304d2cbc61f9473978f53b7e135849
天宇 <617433927@qq.com>
2019-08-20 18:15:36 +0800

    Initial commit

---
+++ app/src/main/java/com/shi/test/androidmvplearningrecord/MainActivity.java
@@ -0,0 +1,20 @@
+package com.shi.test.androidmvplearningrecord;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+  *
+  *
+  *author 施镇杰
+  *email 452239676@qq.com
+  *created 2019/8/20 下午3:36
+  */
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
